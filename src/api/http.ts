db79b6cfea7cd7b23faa696bfa3f12c3cbdb385f
thanvanlong0904 <longthan0409@gmail.com>
2025-11-10 18:26:43 +0700

    ksks

--- src/api/http.ts
+++ src/api/http.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "https://shopapi-b23d.onrender.com/", // đổi theo backend của bạn
+  baseURL: "https://shopapi-production-630e.up.railway.app/", // đổi theo backend của bạn
   headers: {
     "Content-Type": "application/json",
   },
