522b021d0dcc0dc5867d975d34f5f3a7c183a825
thanvanlong0904 <longthan0409@gmail.com>
2025-11-10 16:59:24 +0700

    long22

--- src/api/http.ts
+++ src/api/http.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "http://localhost:3001/", // đổi theo backend của bạn
+  baseURL: "https://shopapi-b23d.onrender.com/", // đổi theo backend của bạn
   headers: {
     "Content-Type": "application/json",
   },
